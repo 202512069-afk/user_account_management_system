c6b4fc22f727f24ee1c7d616d1b31ba0427effbd
202512069-afk <202512069@gordoncollege.edu.ph>
2026-05-04 17:10:18 +0800

    user_account_management_system
    v1

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "user_management_db");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username=? AND password=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo "Login successful!";
+} else {
+    echo "Invalid username or password!";
+}
+?>
