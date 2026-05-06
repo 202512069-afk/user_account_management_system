b3e97bd4db90792e1554c8eb37597a6111c6b8fb
202512069-afk <202512069@gordoncollege.edu.ph>
2026-05-07 02:20:06 +0800

    improved logout confirmation UI

--- User FP2/dashboard.php
+++ User FP2/dashboard.php
@@ -34,7 +34,9 @@ if (!isset($_SESSION['user_id'])) {
                 <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
                     <a href="admin_view.php" class="btn btn-primary admin-panel me-2">Admin Panel</a>
                 <?php endif; ?>
-                <a href="logout.php" class="btn btn-primary" onclick="return confirm('Are you sure you want to logout?')">Logout</a>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#logoutModal">
+    Logout
+</button>
             </div>
         </div>
         <hr>
@@ -106,7 +108,27 @@ if (!isset($_SESSION['user_id'])) {
             </div>
         </div>
     </div>
+<div class="modal fade" id="logoutModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
 
+            <div class="modal-header">
+                <h5 class="modal-title">Confirm Logout</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <div class="modal-body">
+                <p>Are you sure you want to logout?</p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <a href="logout.php" class="btn btn-primary">Logout</a>
+            </div>
+
+        </div>
+    </div>
+</div>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
